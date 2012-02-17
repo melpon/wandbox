10455a9f2d8647d3c5d7102df1ccb5049d90d779
melpon <melt@e-ml.jp>
2012-02-17 12:24:43 +0900

    Implemented EventSource at client side.

--- nyandbox/Handler/Root.hs
+++ nyandbox/Handler/Root.hs
@@ -1,8 +1,8 @@
 module Handler.Root where
 
 import Import
---import Text.Julius (julius)
 import Yesod.Static
+import Network.Wai.EventSource
 
 -- This is a handler function for the GET request method on the RootR
 -- resource pattern. All of your resource patterns are defined in
@@ -19,3 +19,4 @@ getRootR = do
         addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"
         addStylesheet $ StaticR $ StaticRoute ["bootstrap", "css", "bootstrap.min.css"] []
         $(widgetFile "homepage")
+
