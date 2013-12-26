c11c9760117661769604d9e863be91efaddcdfe6
melpon <melt@e-ml.jp>
2013-12-26 09:34:02 +0900

    add Rust

--- kennel/Handler/Root.hs
+++ kennel/Handler/Root.hs
@@ -36,6 +36,7 @@ import Settings.StaticFiles (
     codemirror_mode_php_php_js,
     codemirror_mode_commonlisp_commonlisp_js,
     codemirror_mode_pascal_pascal_js,
+    codemirror_mode_rust_rust_js,
     codemirror_keymap_vim_js,
     codemirror_keymap_emacs_js,
     compiling_gif)
@@ -124,6 +125,7 @@ makeRootR mCodeOutputs = do
         Y.addScript $ StaticR codemirror_mode_php_php_js
         Y.addScript $ StaticR codemirror_mode_commonlisp_commonlisp_js
         Y.addScript $ StaticR codemirror_mode_pascal_pascal_js
+        Y.addScript $ StaticR codemirror_mode_rust_rust_js
         Y.addScript $ StaticR codemirror_keymap_vim_js
         Y.addScript $ StaticR codemirror_keymap_emacs_js
         Y.addStylesheetRemote "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css"
