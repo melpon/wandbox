b12fd956d2c52a88ef092359a729eccd4125ddf7
AIZAWA Hina <hina@bouhime.com>
2015-06-19 15:11:48 +0900

    fix http status code : 500 -> 400, when POST data is not a JSON

--- kennel2/src/kennel.cpp
+++ kennel2/src/kennel.cpp
@@ -184,6 +184,12 @@ public:
             response().status(404);
             return;
         }
+
+        if (request().content_type_parsed().media_type() != "application/json") {
+            response().status(400);
+            return;
+        }
+
         auto value = json_post_data();
         auto protos = make_protocols(value);
 
@@ -316,6 +322,11 @@ public:
     void api_compile() {
         if (request().request_method() != "POST") {
             response().status(404);
+            return;
+        }
+
+        if (request().content_type_parsed().media_type() != "application/json") {
+            response().status(400);
             return;
         }
 
