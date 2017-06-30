f6dffdcc4226db96c58a97bd82f74426993c02e2
melpon <melt@e-ml.jp>
2017-07-01 00:39:43 +0900

    do not build cattleshed on the kennel server

--- ghwebhook.sh
+++ ghwebhook.sh
@@ -9,14 +9,6 @@ cd wandbox/
 git pull
 git submodule update -i
 
-cd cattleshed
-git clean -xfdq .
-autoreconf -i
-CC=/usr/local/gcc-4.8.2/bin/gcc CXX="/usr/local/gcc-4.8.2/bin/g++ -static-libstdc++ -static-libgcc" ./configure --prefix=/usr/local/cattleshed --with-boost=/usr/local/boost/boost-1.57.0/gcc-4.8.2
-make
-echo "exec /usr/local/cattleshed/bin/cattleshed --syslog" > runserver.sh
-cd ../
-
 cd kennel2
 git clean -xdqf .
 ./autogen.sh
@@ -24,10 +16,7 @@ git clean -xdqf .
 make
 cd ../
 '
-make -C ~wandbox/wandbox/cattleshed install
 make -C ~wandbox/wandbox/kennel2 install
 stop kennel2 || true
-stop cattleshed || true
 sleep 1
-start cattleshed
 start kennel2
