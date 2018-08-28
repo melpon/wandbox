2128dfc8c1ae7c15f2c18574a6784ab7448f9cc7
melpon <shigemasa7watanabe+github@gmail.com>
2018-08-28 14:47:41 +0900

    migrate to babel7

---
+++ canine/.babelrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread'
+  ],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-flow'
+  ]
+}
