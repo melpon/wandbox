fa1e701b351e6cae7aca50ab6311563e50064058
melpon <shigemasa7watanabe+github@gmail.com>
2019-09-23 17:00:58 +0900

    libcap 追加

---
+++ cmake/FindCAP.cmake
@@ -0,0 +1,17 @@
+find_path(CAP_INCLUDE_DIR NAMES aio.h)
+find_library(CAP_LIBRARY NAMES rt)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(CAP DEFAULT_MSG CAP_LIBRARY CAP_INCLUDE_DIR)
+
+mark_as_advanced(CAP_INCLUDE_DIR CAP_LIBRARY)
+
+if(CAP_FOUND)
+  if(NOT TARGET CAP::CAP)
+    add_library(CAP::CAP UNKNOWN IMPORTED)
+    set_target_properties(CAP::CAP PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${CAP_INCLUDE_DIR}"
+      IMPORTED_LOCATION "${CAP_LIBRARY}")
+  endif()
+endif()
+
