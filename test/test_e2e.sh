bbef090ee95d63f9d7856fb302524dfecc93bf26
melpon <shigemasa7watanabe+github@gmail.com>
2020-04-15 15:28:04 +0900

    テスト用ポート番号の変更

--- test/test_e2e.sh
+++ test/test_e2e.sh
@@ -34,19 +34,19 @@ trap "kill $CATTLESHED_PID $KENNEL_PID" EXIT
 sleep 2
 
 # コンパイルのテスト
-curl -f -H "Content-type: application/json" -d @assets/test.json  http://localhost:3500/api/compile.json > _tmp/actual_api_compile.json
+curl -f -H "Content-type: application/json" -d @assets/test.json  http://localhost:3600/api/compile.json > _tmp/actual_api_compile.json
 if ! diff -u assets/expected_api_compile.json _tmp/actual_api_compile.json; then
   echo "failed test /api/compile.json" 1>&2
   exit 1
 fi
 
-curl -f -H "Content-type: application/json" -d @assets/test.json  http://localhost:3500/compile > _tmp/actual_compile
+curl -f -H "Content-type: application/json" -d @assets/test.json  http://localhost:3600/compile > _tmp/actual_compile
 if ! diff -u assets/expected_compile _tmp/actual_compile; then
   echo "failed test /compile" 1>&2
   exit 1
 fi
 
-curl -f -H "Content-type: application/json" -d @assets/test.json  http://localhost:3500/api/compile.ndjson > _tmp/actual_api_compile.ndjson
+curl -f -H "Content-type: application/json" -d @assets/test.json  http://localhost:3600/api/compile.ndjson > _tmp/actual_api_compile.ndjson
 if ! diff -u assets/expected_api_compile.json _tmp/actual_api_compile.json; then
   echo "failed test /api/compile.ndjson" 1>&2
   exit 1
@@ -56,11 +56,11 @@ fi
 
 # 以下のような結果から jq する
 #{"permlink":"g0hqobBtg6a3sEfO","program_message":"foo\n","program_output":"foo\n","status":"0","url":"https://wandbox.org/permlink/g0hqobBtg6a3sEfO"}
-PERMLINK=`curl -f -H "Content-type: application/json" -d @assets/test2.json  http://localhost:3500/api/compile.json | jq -r .permlink`
+PERMLINK=`curl -f -H "Content-type: application/json" -d @assets/test2.json  http://localhost:3600/api/compile.json | jq -r .permlink`
 
 # 以下のような結果から jq する
 # {"parameter":{"code":"echo foo","compiler":"bash","compiler-info":{"compiler-option-raw":false,"display-compile-command":"bash prog.sh","display-name":"bash","language":"Bash script","name":"bash","provider":0,"runtime-option-raw":true,"switches":[],"templates":["bash"],"version":"4.4.20(1)-release"},"compiler-option-raw":"","created_at":1586592986,"description":"","github_user":"","is_private":false,"options":"","runtime-option-raw":"","stdin":"","title":""},"result":{"program_message":"foo\n","program_output":"foo\n","status":"0"},"results":[{"data":"Start","type":"Control"},{"data":"foo\n","type":"StdOut"},{"data":"0","type":"ExitCode"},{"data":"Finish","type":"Control"}]}
-OUTPUT=`curl -f http://localhost:3500/api/permlink/$PERMLINK | jq .result.program_message`
+OUTPUT=`curl -f http://localhost:3600/api/permlink/$PERMLINK | jq .result.program_message`
 if [ "$OUTPUT" != '"foo\n"' ]; then
   echo "failed test /permlink" 1>&2
   exit 1
