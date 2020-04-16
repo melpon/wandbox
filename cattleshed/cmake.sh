0f6ffa40b60a128d047d450d19da55e2207336d4
melpon <shigemasa7watanabe+github@gmail.com>
2020-04-16 12:25:23 +0900

    パッケージングとデプロイスクリプトを追加

--- cattleshed/cmake.sh
+++ cattleshed/cmake.sh
@@ -12,9 +12,33 @@ GRPC_DIR="$INSTALL_DIR/grpc"
 CMAKE_BUILD_TYPE=Release
 ENABLE_TSAN=OFF
 ENABLE_ASAN=OFF
+CMAKE_INSTALL_PREFIX="$PROJECT_DIR/_install"
+CMAKE_OPTS=""
 
 while [ $# -ne 0 ]; do
   case "$1" in
+    "--help" )
+      echo "$0 [--tsan] [--asan] [--staging] [--production] [--prefix <dir>] [--help]"
+      exit 0
+      ;;
+
+    "--prefix" )
+      CMAKE_INSTALL_PREFIX="$2"
+      shift 1
+      ;;
+
+    "--staging" )
+      CMAKE_OPTS=" \
+        -DCATTLESHED_STOREDIR=/tmp/cattleshed-staging-log
+        -DCATTLESHED_BASEDIR=/tmp/cattleshed-staging
+        -DCATTLESHED_LISTEN_PORT=50052
+      "
+      ;;
+    "--production" )
+      CMAKE_OPTS=" \
+      "
+      ;;
+
     "--tsan" )
       ENABLE_TSAN=ON
       BUILD_DIR="_build/tsan"
@@ -39,9 +63,10 @@ pushd $BUILD_DIR
     -DSPDLOG_ROOT_DIR="$INSTALL_DIR/spdlog" \
     -DCMAKE_PREFIX_PATH="$INSTALL_DIR/boost;$GRPC_DIR" \
     -DCMAKE_MODULE_PATH=$MODULE_PATH \
-    -DCMAKE_INSTALL_PREFIX=$PROJECT_DIR/_install \
+    -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX \
     -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
     -DENABLE_TSAN=$ENABLE_TSAN \
-    -DENABLE_ASAN=$ENABLE_ASAN
+    -DENABLE_ASAN=$ENABLE_ASAN \
+    $CMAKE_OPTS
   cmake --build . -j`nproc`
 popd
--- kennel2/cmake.sh
+++ kennel2/cmake.sh
@@ -12,9 +12,37 @@ GRPC_DIR="$INSTALL_DIR/grpc"
 CMAKE_BUILD_TYPE=Release
 ENABLE_TSAN=OFF
 ENABLE_ASAN=OFF
+CMAKE_INSTALL_PREFIX=$PROJECT_DIR/_install
+CMAKE_OPTS=""
 
 while [ $# -ne 0 ]; do
   case "$1" in
+    "--help" )
+      echo "$0 [--tsan] [--asan] [--staging] [--production] [--prefix <dir>] [--help]"
+      exit 0
+      ;;
+
+    "--prefix" )
+      CMAKE_INSTALL_PREFIX="$2"
+      shift 1
+      ;;
+
+    "--staging" )
+      CMAKE_OPTS=" \
+        -DKENNEL_GOOGLEANALYTICS=UA-56896607-3 \
+        -DKENNEL_GITHUBCLIENT=f9d429d939d997e6b08e \
+        -DKENNEL_DOMAIN=staging.wandbox.org \
+        -DKENNEL_CATTLESHED_PORT=50052 \
+        -DKENNEL_SERVICE_PORT=3501 \
+      "
+      ;;
+    "--production" )
+      CMAKE_OPTS=" \
+        -DKENNEL_GOOGLEANALYTICS=UA-56896607-3 \
+        -DKENNEL_GITHUBCLIENT=f9d429d939d997e6b08e \
+      "
+      ;;
+
     "--tsan" )
       ENABLE_TSAN=ON
       BUILD_DIR="_build/tsan"
@@ -42,10 +70,11 @@ pushd $BUILD_DIR
     -DSQLite3_INCLUDE_DIR="$INSTALL_DIR/sqlite3/include" \
     -DSPDLOG_ROOT_DIR="$INSTALL_DIR/spdlog" \
     -DCMAKE_PREFIX_PATH="$INSTALL_DIR/boost;$INSTALL_DIR/cppcms;$INSTALL_DIR/cppdb;$INSTALL_DIR/curl;$INSTALL_DIR/sqlite3;$INSTALL_DIR/pcre;$INSTALL_DIR/icu;$GRPC_DIR" \
+    -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX \
     -DCMAKE_MODULE_PATH=$MODULE_PATH \
-    -DCMAKE_INSTALL_PREFIX=$PROJECT_DIR/_install \
     -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
     -DENABLE_TSAN=$ENABLE_TSAN \
-    -DENABLE_ASAN=$ENABLE_ASAN
+    -DENABLE_ASAN=$ENABLE_ASAN \
+    $CMAKE_OPTS
   cmake --build . -j`nproc`
 popd
