e56fb39ed8ecf139e119b10f697f28135a9fefd0
melpon <shigemasa7watanabe+github@gmail.com>
2022-03-05 15:40:21 +0900

    Switch のシリアライズ/デシリアライズしたデータが間違ってたのを修正

--- kennel/src/main.cpp
+++ kennel/src/main.cpp
@@ -21,6 +21,63 @@
 #include "kennel.json.h"
 #include "permlink.h"
 
+namespace wandbox {
+namespace kennel {
+
+// ::wandbox::kennel::Switch
+static void tag_invoke(const boost::json::value_from_tag&, boost::json::value& jv, const ::wandbox::kennel::Switch& v) {
+  boost::json::object obj;
+  obj["type"] = boost::json::value_from(v.type);
+  obj["name"] = boost::json::value_from(v.name);
+  if (v.display_name != decltype(v.display_name)()) {
+    obj["display-name"] = boost::json::value_from(v.display_name);
+  }
+  if (v.display_flags != decltype(v.display_flags)()) {
+    obj["display-flags"] = boost::json::value_from(v.display_flags);
+  }
+  if (v.options != decltype(v.options)()) {
+    obj["options"] = boost::json::value_from(v.options);
+  }
+
+  if (v.type == "single") {
+    obj["default"] = boost::json::value_from(v.single_default);
+  } else {
+    obj["default"] = boost::json::value_from(v.select_default);
+  }
+
+  jv = std::move(obj);
+}
+
+static ::wandbox::kennel::Switch tag_invoke(const boost::json::value_to_tag<::wandbox::kennel::Switch>&, const boost::json::value& jv) {
+  ::wandbox::kennel::Switch v;
+  if (jv.as_object().find("type") != jv.as_object().end()) {
+    v.type = boost::json::value_to<std::string>(jv.at("type"));
+  }
+  if (jv.as_object().find("name") != jv.as_object().end()) {
+    v.name = boost::json::value_to<std::string>(jv.at("name"));
+  }
+  if (jv.as_object().find("display-name") != jv.as_object().end()) {
+    v.display_name = boost::json::value_to<std::string>(jv.at("display-name"));
+  }
+  if (jv.as_object().find("display-flags") != jv.as_object().end()) {
+    v.display_flags = boost::json::value_to<std::string>(jv.at("display-flags"));
+  }
+  if (jv.as_object().find("options") != jv.as_object().end()) {
+    v.options = boost::json::value_to<std::vector<::wandbox::kennel::SelectSwitchOption>>(jv.at("options"));
+  }
+  if (jv.as_object().find("default") != jv.as_object().end()) {
+    if (v.type == "single") {
+      v.single_default = boost::json::value_to<bool>(jv.at("default"));
+    } else {
+      v.select_default = boost::json::value_to<std::string>(jv.at("default"));
+    }
+  }
+  return v;
+}
+
+}
+}
+
 // wandbox::cattleshed::GetVersionResponse を頑張って wandbox::kennel::CattleshedInfo に変換する
 static wandbox::kennel::CattleshedInfo version_response_to_kennel(
     const wandbox::cattleshed::GetVersionResponse& resp) {
@@ -47,14 +104,14 @@ static wandbox::kennel::CattleshedInfo version_response_to_kennel(
         const wandbox::cattleshed::SingleSwitch& ss = s.single();
         sw.type = "single";
         sw.name = ss.name();
-        sw.default_value = ss.default_value();
+        sw.single_default = ss.default_value();
         sw.display_name = ss.display_name();
         sw.display_flags = ss.display_flags();
       } else {
         const wandbox::cattleshed::SelectSwitch& ss = s.select();
         sw.type = "select";
         sw.name = ss.name();
-        sw.default_value = ss.default_value();
+        sw.select_default = ss.default_value();
         for (int k = 0; k < ss.options_size(); k++) {
           wandbox::kennel::SelectSwitchOption option;
           const wandbox::cattleshed::SelectSwitchOption& opt = ss.options(k);
