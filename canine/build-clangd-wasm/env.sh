983acc0f8d9cdbf8aa28e7e3f8076031bdf3d931
melpon <shigemasa7watanabe+github@gmail.com>
2025-03-07 05:07:38 +0900

    Workers ビルドはローカルで clangd ビルドしてやらないと動かないので消しておく

--- canine/build-clangd-wasm/env.sh
+++ canine/build-clangd-wasm/env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # 既にビルドしてあるプロジェクト上で一部だけ更新したい時に使うスクリプト。
-# 変数を出力するので eval `env.sh` して使う。
+# 変数を出力するので eval `./env.sh` して使う。
 set -e
 cd `dirname ${BASH_SOURCE[0]}`
 WORKSPACE_DIR=$PWD
